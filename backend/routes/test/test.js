5355790882f290dfa352c89a9b124f5b39a16687
Jakub-S-K <jakub.stanula.kaczka@gmail.com>
2024-12-08 22:21:27 +0100

    [Backend][feat] Schemat tabeli, relacje i przykladowy endpoint
    Zdefiniowanie wszystkich tabeli
    Dodanie relacji z wszystkimi tabelami
    Przykladowy endpoint /api/test/user/:id/comments

--- backend/routes/test/test.js
+++ backend/routes/test/test.js
@@ -1,4 +1,5 @@
 const User = require('../../schema.js').User;
+const Comment = require('../../schema.js').Comment;
   
 module.exports.get = function (req, res) {
     console.log("Welcome welcome from vibestream API");
@@ -21,6 +22,23 @@ module.exports.test_db = async function (req, res) {
     const user = await User.findAll();
     console.log(user);
     res.json(user);
+}
+
+module.exports.get_comments = async function (req, res) {
+    _id = req.params.id //expressJS parsuje automatycznie wszystkie zmienne w adresie czyli /:id/ jest pod req.params.id
+    const user = await User.findOne({ //Dla wielu rekordów jest jeszcze findAll. await czeka na zakonczenie requesta z bazy.
+        where: {
+            id: _id //where jak w sql tutaj id uzytkownika z bazy jest porownywane
+        },
+        include: Comment //LEFT JOIN jak w sql, do inner join trzeba zrobić -> include: [{ model: Comment, required: true }]
+    });
+    if (!user) {
+        console.log('Not found');
+        res.status(404).send("Not found.");
+        return;
+    }
+    console.log(user);
+    res.json(user); //znaleziony rekord jest przerabiany na jsona i zwracany.
 }
 
 const artistsJSON = [
