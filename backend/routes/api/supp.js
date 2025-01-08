06925bd8b7cfc9dfa16e408e1e899eee3a1da71c
Mydlitor <jakub.sputo@gmail.com>
2025-01-08 15:00:28 +0100

    [backend] [feat] GET api/genres

--- backend/routes/api/supp.js
+++ backend/routes/api/supp.js
@@ -1,4 +1,5 @@
 const Tag = require('../../schema.js').Tag;
+const Genre = require('../../schema.js').Genre;
 const sequelize = require('../../db_conn.js').conn;
 
 module.exports.tags = async function (req, res) {
@@ -11,4 +12,16 @@ module.exports.tags = async function (req, res) {
     }
     console.log(tags);
     res.json(tags);
+}
+
+module.exports.genres = async function (req, res) {
+    const genres = await Genre.findAll({
+        attributes: ['name']
+    })
+    if (!genres) {
+        res.status(500).send("Internal Server Error");
+        return;
+    }
+    console.log(genres);
+    res.json(genres);
 }
