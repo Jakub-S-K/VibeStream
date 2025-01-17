34018e4081b3eca36d83d436987938254036349e
R3sssque <108767799+R3sssque@users.noreply.github.com>
2025-01-17 23:11:10 +0100

    Add users in init_db_data.js and upload their avatars to directory

--- backend/init_db_data.js
+++ backend/init_db_data.js
@@ -1,19 +1,39 @@
-module.exports.test_users = "INSERT INTO `user` (`id`, `nickname`, `email`, `password`, `creation_date`, `bio`) VALUES\
-(1, 'MusicLover', 'musiclover@example.com', 'password123', '2024-12-19 10:43:44', 'Passionate about discovering new indie tracks.'),\
-(2, 'BeatMaker99', 'beatmaker99@example.com', 'securePass99', '2024-12-19 10:43:44', 'Aspiring producer sharing my beats with the world.'),\
-(3, 'SoundHunter', 'soundhunter@example.com', 'hunterPass456', '2024-12-19 10:43:44', 'Exploring the depths of new and unique sounds.'),\
-(4, 'VibeMaster', 'vibemaster@example.com', 'vibe2024!', '2024-12-19 10:43:44', 'Sharing vibes and playlists for every mood.'),\
-(5, 'MelodySeeker', 'melodyseeker@example.com', 'melodyPass789', '2024-12-19 10:43:44', 'On a journey to find the perfect melody.'),\
-(6, 'PodwojnaLipa', 'podwojnalipa@gmail.com', 'P@ssw0rd2024', '2024-12-19 10:51:47', 'Lubię relaksować się przy dobrej muzyce i odkrywać nowe brzmienia.'),\
-(7, 'DJKaprysnaDrukarkaHP', 'djkaprysna@outlook.com', 'Druk@rka123', '2024-12-19 10:51:47', 'Tworzę muzykę inspirowaną dźwiękami codziennego życia.'),\
-(8, 'patrycjaaaaaa', 'patrycja123@yahoo.com', 'P@trycja2024!', '2024-12-19 10:51:47', 'Fanka akustycznych brzmień i chilloutowych playlist.'),\
-(9, 'kierowcaSolarisa', 'kierowca.solaris@interia.pl', 'S0laris2024!', '2024-12-19 10:51:47', 'Muzyka to moja codzienna towarzyszka podczas długich tras.'),\
-(10, 'DJPaprykarzSzczecinski', 'djpaprykarz@gmail.com', 'P@pryk@rz1!', '2024-12-19 10:51:47', 'DJ z pasją do miksowania nieoczywistych gatunków.'),\
-(11, 'LeoLoud', 'leoloud@protonmail.com', 'L3oL0ud!2024', '2024-12-19 10:51:47', 'Eksperymentuję z dźwiękiem i szukam nowych muzycznych wyzwań.'),\
-(12, 'JakeJam', 'jakejam@icloud.com', 'J@k3J@ms2024', '2024-12-19 10:51:47', 'Producent muzyczny z zamiłowaniem do groovy bitów.'),\
-(13, 'babanarowerze', 'babanarowerze@gmail.com', 'Bab@N@2024', '2024-12-19 10:54:36', 'Muzyka to mój najlepszy towarzysz podczas rowerowych przejażdżek!'),\
-(14, 'soundwave42', 'soundwave42@gmail.com', 'S0undW@v3!2024', '2024-12-19 10:55:55', 'Pasjonat nowych brzmień i eksperymentów w muzyce elektronicznej.'),\
-(15, 'beatmasterx', 'beatmasterx@yahoo.com', 'B3@tM@st3rX', '2024-12-19 10:55:55', 'Tworzę hip-hopowe bity, zawsze w poszukiwaniu idealnego flow.');\
+// module.exports.test_users = "INSERT INTO `user` (`id`, `nickname`, `email`, `password`, `creation_date`, `bio`) VALUES\
+// (1, 'MusicLover', 'musiclover@example.com', 'password123', '2024-12-19 10:43:44', 'Passionate about discovering new indie tracks.'),\
+// (2, 'BeatMaker99', 'beatmaker99@example.com', 'securePass99', '2024-12-19 10:43:44', 'Aspiring producer sharing my beats with the world.'),\
+// (3, 'SoundHunter', 'soundhunter@example.com', 'hunterPass456', '2024-12-19 10:43:44', 'Exploring the depths of new and unique sounds.'),\
+// (4, 'VibeMaster', 'vibemaster@example.com', 'vibe2024!', '2024-12-19 10:43:44', 'Sharing vibes and playlists for every mood.'),\
+// (5, 'MelodySeeker', 'melodyseeker@example.com', 'melodyPass789', '2024-12-19 10:43:44', 'On a journey to find the perfect melody.'),\
+// (6, 'PodwojnaLipa', 'podwojnalipa@gmail.com', 'P@ssw0rd2024', '2024-12-19 10:51:47', 'Lubię relaksować się przy dobrej muzyce i odkrywać nowe brzmienia.'),\
+// (7, 'DJKaprysnaDrukarkaHP', 'djkaprysna@outlook.com', 'Druk@rka123', '2024-12-19 10:51:47', 'Tworzę muzykę inspirowaną dźwiękami codziennego życia.'),\
+// (8, 'patrycjaaaaaa', 'patrycja123@yahoo.com', 'P@trycja2024!', '2024-12-19 10:51:47', 'Fanka akustycznych brzmień i chilloutowych playlist.'),\
+// (9, 'kierowcaSolarisa', 'kierowca.solaris@interia.pl', 'S0laris2024!', '2024-12-19 10:51:47', 'Muzyka to moja codzienna towarzyszka podczas długich tras.'),\
+// (10, 'DJPaprykarzSzczecinski', 'djpaprykarz@gmail.com', 'P@pryk@rz1!', '2024-12-19 10:51:47', 'DJ z pasją do miksowania nieoczywistych gatunków.'),\
+// (11, 'LeoLoud', 'leoloud@protonmail.com', 'L3oL0ud!2024', '2024-12-19 10:51:47', 'Eksperymentuję z dźwiękiem i szukam nowych muzycznych wyzwań.'),\
+// (12, 'JakeJam', 'jakejam@icloud.com', 'J@k3J@ms2024', '2024-12-19 10:51:47', 'Producent muzyczny z zamiłowaniem do groovy bitów.'),\
+// (13, 'babanarowerze', 'babanarowerze@gmail.com', 'Bab@N@2024', '2024-12-19 10:54:36', 'Muzyka to mój najlepszy towarzysz podczas rowerowych przejażdżek!'),\
+// (14, 'soundwave42', 'soundwave42@gmail.com', 'S0undW@v3!2024', '2024-12-19 10:55:55', 'Pasjonat nowych brzmień i eksperymentów w muzyce elektronicznej.'),\
+// (15, 'beatmasterx', 'beatmasterx@yahoo.com', 'B3@tM@st3rX', '2024-12-19 10:55:55', 'Tworzę hip-hopowe bity, zawsze w poszukiwaniu idealnego flow.');\
+// "
+
+// All users have the default password '123456'
+module.exports.test_users = "\
+INSERT INTO `user` (`id`, `nickname`, `email`, `password`, `creation_date`, `bio`) VALUES\
+('04983039-5421-4c66-9f63-3fac8f43e78d', 'DJKaprysnaDrukarkaHP', 'djkaprysna@drukarkahp.pl', '$2a$10$xbrrHoZQLHmTvvLP5hrJPOwoMACEjaJUcAdQDuAvbnA3lp4APEtCy', '2025-01-17 20:15:21', 'HP Smart lover'),\
+('210c5b29-1d7d-452f-a8e4-549bc5b7650b', 'President Duda', 'president@duda.pl', '$2a$10$OeHGl1ZCEVSyDaQx8B3A0ezFUGrQWnV2D5x/rJ.lz7radKRUQlqKS', '2025-01-17 21:33:19', 'Serek President i pasztet Duda'),\
+('22a4812f-dcda-4360-a7f3-9bcff6a7b389', 'MelodySeeker', 'melodyseeker@gmail.com', '$2a$10$SPmLBREv7bZnsOt.ooYHRuHO7xqwdMpLO2iAi6rbnBgtWTnBXITom', '2025-01-17 21:15:22', 'A passionate seeker of sounds, always on the hunt for new musical journeys.'),\
+('37daf514-8122-4d53-b72d-3ec2a4018117', 'patrycjaaaaa', 'patrycja123@yahoo.com', '$2a$10$hAIVBrRd7TU.eYmB9zPE.u.TVQfldn4QgfUPQJ//i96SxLWVjODlO', '2025-01-17 20:17:26', 'Hej tu Patrycja'),\
+('4a0b05eb-3d8a-4182-bfa2-5ddae7c4990a', 'meegas', 'meegas@gmail.com', '$2a$10$cJQmNIzWMCDLGhZcTIlOaeStNJlC0SMYIDh5HQ2lMUG3JxhmWYTVG', '2025-01-17 20:19:48', 'Szukam dźwięków tak głośnych, jak praca młotów w hucie.'),\
+('545c6907-1ca2-4a26-a530-33501714e6ab', 'kierowcaSolarisa', 'kierowca.solarisa@interia.pl', '$2a$10$cYA3Shm51LcDjv0Gb/D90O/zfhy1jnShgwuZWzgDa4NisgcaOmxo2', '2025-01-17 21:12:48', 'Proszę się trzymać, bo zaraz wjeżdżam z dropem!'),\
+('632316eb-ae56-417e-a931-9382015d4c70', 'NightGroover', 'nightgroover@yahoo.com', '$2a$10$RAlqyh8MFTbFK9jbCXitMOtkNXVIvi.DezfX63QQlrWSt.qVGNdZ.', '2025-01-17 21:25:15', 'Lost in the lights, moving to the night’s rhythm.'),\
+('6436f3b8-eb42-48e2-b0ce-fe9fdbeb0861', 'babanarowerze', 'babanarowerze@gmail.com', '$2a$10$X6F9mjqPfmtMN8OpkKaBVuHg64zhLyHy6R4TrVTstZaawv5ArjvTm', '2025-01-17 20:14:33', 'baba na rowerze to poważna sprawa'),\
+('7e31eba6-0095-4770-b11f-89e29a5c7813', 'DJPaprykarzSzczecinski', 'djpaprykarz@szczecinski.pl', '$2a$10$uB4MvAsrPMAz1U/rfs4HBOnCF7jKrWWIwiVtlpxyWCwGgiU6JZLDa', '2025-01-17 20:16:07', 'Bity dobre jak świeży paprykarz prosto z puszki.'),\
+('91ab6db4-9b0f-4a70-930b-283bbc1b79a7', 'PodwojnaLipa', 'podwojnalipa@gmail.com', '$2a$10$JNDWSfWTohMwhQdxY2hIq.iZhVGFjwacmFzbYa832p.uj7aHI8fuS', '2025-01-17 20:16:43', 'aka podwójny przypał aka dwojaka padaka'),\
+('c2b8ba43-be57-4671-9ddf-d762ed69c5aa', 'beatmasterx', 'beatmasterx@yahoo.com', '$2a$10$HaJpMjjEXUM6m/.pUUWTHuy4EEcnOH93hoj0prh0Hyx1HdaqVs3sC', '2025-01-17 20:20:23', NULL),\
+('d263b2c9-edc9-411e-a28c-a955902615a3', 'LeoLoud', 'leoloud@protonmail.com', '$2a$10$1hA0JL05.rclO44bdewN.ONU/Zmb0lBv6h2LAYmXeixJfltNhd5ze', '2025-01-17 21:19:44', 'Bold vibes, loud beats, endless energy.'),\
+('d42f6716-304b-44db-b4bf-f77d3e6d0220', 'VibeMaster', 'vibemaster@yahoo.com', '$2a$10$bVTxfNmPzkPqpY3Qw3RkXe3TbpJrdNV8lmMZz4jEcaREC3tW.5fym', '2025-01-17 20:21:02', NULL),\
+('e12906df-cfdb-472c-b93b-ce34a20d0e8d', 'SoundHunter', 'soundhunter@gmail.com', '$2a$10$LTBuoHn8zHADMqsEGgpO..QQsBlYpaf/b6U0UmDLAHICA2cERvxgC', '2025-01-17 20:19:14', 'Exploring the depths of new and unique sounds.'),\
+('f9ec03e5-9bce-4a29-8f9d-4f15a45b7c60', 'pralkosuszarka', 'pralko@suszarka.pl', '$2a$10$hw5VDsDsJAN/gUpgYkCiteUs.NrbnhwWoCVIcxYKH1juLwIFZsij.', '2025-01-17 21:35:28', 'Słucham dźwięków przypominających cegłę wirującą w pralce – absurdalne, ale intrygujące.');\
 "
 
 module.exports.test_tags = "\
